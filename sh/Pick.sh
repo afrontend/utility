56017a98464a947cc5179c46bd63e6ad8c5c88c1
afrontend <frontend.wordpress@gmail.com>
2016-10-25 22:31:46 +0900

    여러 인자를 하나의 명령어로 인식하도록 변경

--- sh/Pick.sh
+++ sh/Pick.sh
@@ -4,22 +4,7 @@ command -v pick >/dev/null 2>&1 || { echo >&2 "I require pick but it's not insta
 trap 'echo you hit Ctrl-C/Ctrl-\, now exiting..; exit' SIGINT SIGQUIT
 TMP_SCRIPT_NAME=$(mktemp)
 
-function _runScript() {
-    sed "s/^/$1 /" | pick > $TMP_SCRIPT_NAME
-    cat $TMP_SCRIPT_NAME
-    chmod u+x $TMP_SCRIPT_NAME
-    $TMP_SCRIPT_NAME
-}
-
-me=`basename $0`
-case $1 in
-    '')
-        _runScript cat
-        ;;
-    '-h')
-        echo "ls -1 | $me cat"
-        ;;
-    *)
-        _runScript $1
-        ;;
-esac
+sed "s/^/$* /" | pick > $TMP_SCRIPT_NAME
+cat $TMP_SCRIPT_NAME
+chmod u+x $TMP_SCRIPT_NAME
+$TMP_SCRIPT_NAME
