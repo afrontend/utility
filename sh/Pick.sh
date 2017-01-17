0aca8882d6987c2f2965f932bbdc6953f67a05c0
afrontend <frontend.wordpress@gmail.com>
2017-01-17 10:39:39 +0900

    shellcheck 적용하여 경고 제거

--- sh/Pick.sh
+++ sh/Pick.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Filename: Pick.sh
+#Filename: Pick.sh
 command -v pick >/dev/null 2>&1 || { echo >&2 "I require pick but it's not installed.  Aborting."; exit 1; }
 trap 'echo you hit Ctrl-C/Ctrl-\, now exiting..; exit' SIGINT SIGQUIT
 TMP_SCRIPT_NAME=$(mktemp)
 
-sed "s/^/$* /" | pick > $TMP_SCRIPT_NAME
-cat $TMP_SCRIPT_NAME
-chmod u+x $TMP_SCRIPT_NAME
+sed "s/^/$* /" | pick > "$TMP_SCRIPT_NAME"
+cat "$TMP_SCRIPT_NAME"
+chmod u+x "$TMP_SCRIPT_NAME"
 $TMP_SCRIPT_NAME
